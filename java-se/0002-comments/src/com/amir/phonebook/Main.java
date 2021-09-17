9bf0d44845f5f39786e4377ecdb9461bb4e3096b
amir <ahosseinnemat@gmail.com>
2021-09-17 18:34:31 +0430

    comment sample

---
+++ java-se/0002-comments/src/com/amir/phonebook/Main.java
@@ -0,0 +1,20 @@
+package com.amir.phonebook;
+
+/**
+ * this is a sample document
+ * @author amir
+ */
+
+/*  this program just print a text
+    with using println
+ */
+public class Main {
+
+    // main method
+    public static void main(String[] args) {
+        // print a text
+        System.out.println("This is a simple text.");
+
+    } // end of the main method
+
+} // end of the class
